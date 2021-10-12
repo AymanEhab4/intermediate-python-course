254b412276f19c1f5d1b06422fdc9dbb6254cc8d
AymanEhab4 <aymanehab660@gmail.com>
2021-10-12 11:00:19 -0700

    Success in the 4th try :)

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,13 @@
 import random
 
-# def main():
-roll = random.randint(1,6)
-print(f"You rolled a {roll}")
+def main():
+  roll = random.randint(1,6)
+  if roll == 1:
+    print(f"You rolled a {roll} Critical Failed :(")
+  elif roll == 6:
+    print(f"You rolled a {roll} Critical Success :)")
+  else:
+    print(f"You rolled a {roll}")
 
-# if __name__== "__main__":
-#   main()
+if __name__== "__main__":
+  main()
